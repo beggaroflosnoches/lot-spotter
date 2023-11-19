cb1f29b74d751480eb5e81f4fc8807d345a6bfe9
Henry Pham <henrypham0214@gmail.com>
2023-11-19 14:42:36 -0500

    Added additional HTML pages

--- lot.js
+++ lot.js
@@ -28,3 +28,66 @@ spaceElements.forEach((space) => {
         }
     });
 });
+
+// Retrieve data from database
+// const { DynamoDBClient, ScanCommand } = require("@aws-sdk/client-dynamodb");
+
+// const dbClient = new DynamoDBClient({ region: 'us-east-2' });
+
+// const param = {
+//     TableName: 'lot1',
+// };
+
+// const command = new ScanCommand(param);
+
+// dbClient.send(command)
+//     .then((data) => {
+//         const items = data.Items;
+//         items.forEach((item, index) => {
+//             const id = item.space_id.N;
+//             const hasCar = item.is_full.B;
+
+//             console.log(`${id},${hasCar}`);
+//         });
+//     })
+//     .catch((error) => {
+//         console.error("Error retrieving items:", error);
+//     });
+
+// Example data
+const databaseData = [
+    { id: 0, hasCar: true },
+    { id: 3, hasCar: true },
+    { id: 4, hasCar: true },
+];
+
+// Function to update HTML elements based on databaseData
+function updateHTMLFromDatabase() {
+    databaseData.forEach((item) => {
+        const id = item.id; 
+        const hasCar = item.hasCar;
+
+        // Match id
+        const targetSpaceElement = document.querySelector(`.space[data-space-id="${id}"]`);
+
+        if (targetSpaceElement) {
+            // Update data-has-car attribute
+            targetSpaceElement.setAttribute('data-has-car', hasCar);
+
+            const carImg = targetSpaceElement.querySelector('img');
+            const lotHeader = targetSpaceElement.querySelector('h6');
+
+            // Update display
+            if (hasCar) {
+                carImg.style.display = 'block'; // Show car
+                lotHeader.style.display = 'none'; // Hide text
+            } else {
+                carImg.style.display = 'none'; // Hide car
+                lotHeader.style.display = 'block'; // Show text
+            }
+        }
+    });
+}
+
+// Call function
+updateHTMLFromDatabase();
